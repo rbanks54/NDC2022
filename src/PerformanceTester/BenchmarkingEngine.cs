d0abbb4c261a691fd503376d15ab9aad9e4989a4
Richard Banks <rbanks54@msn.com>
2022-07-18 12:58:25 +1000

    Add performance tester shell

---
+++ src/PerformanceTester/BenchmarkingEngine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using UglyToad.PdfPig;
+
+namespace PerformanceTester
+{
+    class BenchmarkingEngine
+    {
+        private readonly string fileName;
+
+        public BenchmarkingEngine(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void Start()
+        {
+            var timer = Stopwatch.StartNew();
+
+            using PdfDocument document = PdfDocument.Open(fileName);
+
+            var structure = document.Structure;
+            int pageCount = document.NumberOfPages;
+
+            int wordCount = 0;
+            for (var p = 1; p <= pageCount; p++)
+            {
+                var page = document.GetPage(p);
+                wordCount += page.GetWords().Count();
+            }
+
+            timer.Stop();
+
+            Console.WriteLine($"Pages: \t{pageCount}");
+            Console.WriteLine($"Words: \t{wordCount}");
+            Console.WriteLine($"Duration: \t{timer.Elapsed.TotalSeconds}s");
+        }
+
+    }
+}
---
+++ src/PerformanceTester/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PerformanceTester
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var pdf = @"C:\src\PdfPig\src\PerformanceTester\pdfs\homers illiad.pdf";
+            // var pdf = @"C:\src\PdfPig\src\PerformanceTester\pdfs\War and Peace.pdf";
+
+            new BenchmarkingEngine(pdf).Start();
+        }
+    }
+}
