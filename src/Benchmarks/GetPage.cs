fb1d4c205306f5f9bab295131e034506b8a5d627
Richard Banks <rbanks54@msn.com>
2022-07-18 15:08:18 +1000

    getPage benchmark

---
+++ src/Benchmarks/GetPage.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser]
+public class GetPage
+{
+    private PdfDocument? theIliad;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        theIliad = PdfDocument.Open(@"C:\src\PdfPig\src\PerformanceTester\pdfs\homers illiad.pdf");
+    }
+
+    [GlobalCleanup]
+    public void CleanUp()
+    {
+        theIliad?.Dispose();
+    }
+
+    [Params(1, 20, 50)]
+    public int PageNumber { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public Page Baseline()
+    {
+        return theIliad!.GetPage(PageNumber);
+    }
+}
---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher
+    .FromAssembly(typeof(Program).Assembly)
+    .Run(args,DefaultConfig.Instance);
